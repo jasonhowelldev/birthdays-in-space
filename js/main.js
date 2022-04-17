7bde818ef2c79d75393f7c775ebf4ffed418b8d7
Jason <jhowell463@gmail.com>
2022-04-17 15:12:04 -0400

    simplified date comparisons and no longer send api requests for future dates

--- js/main.js
+++ js/main.js
@@ -27,79 +27,49 @@ function viewImages(){
     const birthdayYear = parseInt(inputDate.substring(0,4))
     const birthdayMonth = parseInt(inputDate.substring(5,7))
     const birthdayDay = parseInt(inputDate.substring(8,10))
-    
-    console.log(`birthday : ${birthdayYear}-${birthdayMonth}-${birthdayDay}`)
 
     // need to get the image for every year from their birthday to today
 
     /// first, get the api url for each year in an array
-
-    // // TODO : check to see if birthday has happened yet this year, if not, skip it
-    // // TODO : started June 16, 1995 so anything before that will be undefined
     let urls = []
     for ( i = 0; i+birthdayYear <= todayYYYY; i++) {
         let shouldAddRequest = true
-        
-        // check if birthday occurs before June 16, 1995
-        if ( i+birthdayYear <  1995) {
-            console.log("year is before 1995, too early for images to start")
-            shouldAddRequest = false
-        }else if ( i+birthdayYear ==  1995 && birthdayMonth < 06) {
-            console.log("before June on 1995, images have not started yet")
-            shouldAddRequest = false
-        }else if ( i+birthdayYear ==  1995 && birthdayMonth == 06 && birthdayDay < 16) {
-            console.log("June of 1995, but is it before the 16th, images have not started yet")
-            shouldAddRequest = false
-        }
 
-        // check if birthday is after current date
-        console.log(`compare dates ${inputDate} > ${today}`)
-        if ( inputDate > today ) {
+        if ( new Date(i+birthdayYear,birthdayMonth-1,birthdayDay).getTime() < new Date(1995,05,16).getTime()) {
+            console.log("the target date is before June 16,1995, before when NASA started uploading images")
+            shouldAddRequest = false
+        }else if ( new Date(i+birthdayYear,birthdayMonth-1,birthdayDay).getTime() > new Date(todayPretty).getTime()) {
             console.log("the input date is after today's date")
             shouldAddRequest = false
         }
     
-        //console.log(`loop : ${i}  and current loop year : ${i+birthdayYear}`)
         if ( shouldAddRequest ) {
             urls[i] = `https://api.nasa.gov/planetary/apod?api_key=${localStorage.getItem('apiKey')}&date=${i+birthdayYear}-${birthdayMonth}-${birthdayDay}`
         }
         
     }
 
-    /// now, store the data received from each url into an array
-    let yearlyData = []
-
+    /// make each api call and append data to the DOM
     urls.forEach((url, i) => {
         let li = document.createElement('li')
         li.id = url
         
-        
         document.querySelector('ul').appendChild(li)
-        
 
             fetch(url)
             .then(response => response.json())
             .then(data => {
-                //console.log(data)
-                yearlyData[i] = data
-
-                //const li = document.createElement('li')
                 li.textContent = `${data.date} : ${data.title}`
 
                 let img = document.createElement('img')
+                // TODO : set image size in css
                 img.src = data.url
                 img.width = 256
                 img.height = 256
                 document.getElementById(url).appendChild(img)
-                
-                //document.querySelector('ul').appendChild(li)
-
         })
 
     })
 
-    // check to see if their birthday has happened this year, if not say something like
-    // " who know what NASA might on your next birthday in <numberOfDaysUntilNextBirthday> days" 
-    
 }
 
